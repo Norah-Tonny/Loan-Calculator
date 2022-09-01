f661cd2ba10bd09b2db88c00067f9146b86063a6
Norah-Tonny <norahtonny75@gmail.com>
2022-09-01 11:35:31 +0300

    fixed UI part

--- Js/app.js
+++ Js/app.js
@@ -1,25 +1,30 @@
 
 const amount = document.getElementById("amount");
-    const interest_rate = document.getElementById("interest_rate");
-    const months = document.getElementById("months").value;
-    const years= document.getElementById("years");
-    const started_date =document.getElementById("started_date");
-    const deposit = document.getElementById("deposit");
-    const extra_fee =document.getElementById("extra_fee");
+const interest_rate = document.getElementById("interest_rate");
+const months = document.getElementById("months").value;
+const years = document.getElementById("years");
+const started_date = document.getElementById("started_date");
+const deposit = document.getElementById("deposit");
+const extra_fee = document.getElementById("extra_fee");
 const baloon_end = document.getElementById("balloon_end");
-const button=document.getElementById("calc")
-    
+const button = document.getElementById("calc")
+const value_number=document.getElementById("value_number")
+
+
 function loancalculator() {
-const amountValue=parseInt(amount.value)
-const interestValue=parseInt(interest_rate.value)
-    const interest = (amountValue * (interestValue/100));
+    const amountValue = parseInt(amount.value)
+    const interestValue = parseInt(interest_rate.value)
+    const interest = (amountValue * (interestValue / 100));
     const loanPayment = ((amountValue) + interest).toFixed(2);
-    const depositValue=((amountValue)-interest)
+    const depositValue = ((amountValue) - interest)
     // document.getElementById("payment").innerHTmml = "monthly Payment=$" + payment;
-    console.log(depositValue)
+    console.log(loanPayment)
+   
 }
 
-button.addEventListener("click", (e) =>{
+button.addEventListener("click", (e) => {
     loancalculator()
+    value_number.innerHTML=""
+
 })
 
